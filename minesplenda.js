c136e982b631de7365133c0849d9cfba1817eaf2
Alvarito050506 <donfrutosgomez@gmail.com>
2020-08-20 12:15:11 -0300

    Basic optimization.

--- minesplenda.js
+++ minesplenda.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const mineflayer = require('mineflayer');
 //const config = require('./config.json');
+
 var prefix = "!";
 const bot = mineflayer.createBot({
-
   host: '127.0.0.1', // optional
   port: 25577,       // optional
   username: 'user@gmail.com', // email and password are required only for
@@ -12,8 +12,16 @@ const bot = mineflayer.createBot({
   version: '1.16.1'                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
 })
 
+var commands = {
+  "timeset": "time set",
+  "teleport": "tp",
+  "kick": "kick",
+  "ban": "ban",
+  "say": "say"
+};
+
 bot.on('chat', function (username, message) {
-  if (username === bot.username) return
+  if (username === bot.username) return;
   bot.chat(message)
 })
 
@@ -31,40 +39,12 @@ client.on('message', message => {
 
   const args = message.content.slice(prefix.length).trim().split(' ');
   const command = args.shift().toLowerCase();
-  if (command === 'timeset') {
-	if (!args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+
+  if (!args.length) {
+    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
-
-	bot.chat(`/time set ${args}`);
-  }
-  else if (command === 'kick') {
-        if (!args.length) {
-                return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-
-        bot.chat(`/kick ${args}`);
-  }
-  else if (command === 'ban') {
-        if (!args.length) { 
-                return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-
-        bot.chat(`/ban ${args}`);
-  }
-  else if (command === 'teleport') {
-        if (!args.length) { 
-                return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-
-        bot.chat(`/tp ${args}`);
-  }
-  else if (command === 'say') {
-        if (!args.length) { 
-                return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-
-        bot.chat(`/say ${args}`);
+  if (commands[command] != undefined) {
+    bot.chat(`/${commands[command]} ${args}`);
   }
 });
 
