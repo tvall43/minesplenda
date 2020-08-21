1defe6d6db00fadaa7216776c2dafaf33181ba52
tim <tvall43@gmail.com>
2020-08-21 09:17:30 -0500

    basically done. just needs perms actually set

--- minesplenda.js
+++ minesplenda.js
@@ -39,6 +39,9 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(' ');
   const command = args.shift().toLowerCase();
   if (command === 'timeset') {
+        if(!message.member.roles.cache.some(r=>["Administrator", "Moderator"].includes(r.name)))
+          return message.reply("Sorry, you don't have permissions to use this!");
+
 	if (!args.length) {
 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
@@ -46,6 +49,9 @@ client.on('message', message => {
 	bot.chat(`/time set ${args}`);
   }
   else if (command === 'kick') {
+        if(!message.member.roles.cache.some(r=>["Administrator", "Moderator"].includes(r.name)))
+          return message.reply("Sorry, you don't have permissions to use this!");
+
         if (!args.length) {
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
@@ -53,6 +59,9 @@ client.on('message', message => {
         bot.chat(`/kick ${args}`);
   }
   else if (command === 'ban') {
+        if(!message.member.roles.cache.some(r=>["Administrator", "Moderator"].includes(r.name)))
+          return message.reply("Sorry, you don't have permissions to use this!");
+
         if (!args.length) { 
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
@@ -60,19 +69,27 @@ client.on('message', message => {
         bot.chat(`/ban ${args}`);
   }
   else if (command === 'teleport') {
+        if(!message.member.roles.cache.some(r=>["Administrator", "Moderator"].includes(r.name)))
+          return message.reply("Sorry, you don't have permissions to use this!");
+
         if (!args.length) { 
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
         const tpStuff = args.join(" ");
-        bot.chat(`/tp ` tpStuff);
+        bot.chat(`/tp ` + tpStuff);
   }
   else if (command === 'say') {
+        if(!message.member.roles.cache.some(r=>["Administrator", "Moderator"].includes(r.name)))
+          return message.reply("Sorry, you don't have permissions to use this!");
+
         if (!args.length) { 
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
         const sayMessage = args.join(" ");
         bot.chat(`/say ` + sayMessage);
   }
+
+
   else if (command === 'minesplenda') {
         message.channel.send(`im alive`);
   }
