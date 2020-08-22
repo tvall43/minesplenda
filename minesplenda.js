0bee9290f70fffea538c47c6daea7239eeacaf4c
SugarD-x <sugajack2004@gmail.com>
2020-08-22 01:22:32 -0700

    Revert "Added missing semi-colons, fixed status message code"
    This reverts commit fd6622bdf3295f569279cb4a3bcb652bf477a1f8.

--- minesplenda.js
+++ minesplenda.js
@@ -10,24 +10,27 @@ const bot = mineflayer.createBot({
   username: 'user@gmail.com', // email and password are required only for
   password: 'N0t4R3aLpA$$',          // online-mode=true servers
   version: '1.16.1'                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
-});
+})
 
 bot.on('chat', function (username, message) {
   if (username === bot.username) return
   bot.chat(message)
-});
+})
 
 // Log errors and kick reasons:
-bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn));
-bot.on('error', err => console.log(err));
+bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
+bot.on('error', err => console.log(err))
 
 client.on("ready", async =>{
-  console.log("discord");
+  console.log("discord")
   client.user.setPresence({
         status: "online",  //You can show online, idle....
+        game: {
+            name: "Minecraft, duh",  //The message shown
+            type: "PLAYING:" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
   });
-  client.user.setActivity('Minecraft, duh', { type: 'PLAYING' });
-});
+})
 
 client.on('message', message => {
 
